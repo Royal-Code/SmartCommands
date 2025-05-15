11156765c5033082362a86360615b4cfaffbdf78
eglauko <eglauko@hotmail.com>
2025-05-15 18:16:34 -0300

    Atualização do projeto e reestruturação de código
    Abaixo estão as principais mudanças realizadas:
    
    * Referências de Projeto
      - Adicionada referência ao projeto `RoyalCode.SmartCommands.Tests.Models.csproj`.
      - Modificada referência para `RoyalCode.SmartCommands.Generators.csproj` com novos atributos.
    
    * Estrutura do Projeto
      - Adicionado novo item de pasta "Commands" no projeto Demo.
      - Criado o arquivo `RoyalCode.SmartCommands.Tests.Models.csproj` com configurações adequadas.
    
    * Reintrodução de Código
      - Reintroduzido código em várias classes com formatação e estrutura adequadas.
      - Garantida a inclusão correta de referências de pacotes no projeto de testes.
    
    * Atualização da Solução
      - Atualizado o arquivo `SmartCommands.sln` para incluir o novo projeto.
      - Adicionada configuração global para o novo projeto na solução.

--- src/RoyalCode.SmartCommands.Tests.Models/CriarProduto.cs
+++ src/RoyalCode.SmartCommands.Tests.Models/CriarProduto.cs
@@ -17,13 +17,12 @@ public partial class CriarProduto
             .HasProblems(out problems);
     
         return result;
-        //return result ? 1 : 0;
     }
 
     [Command, WithValidateModel, WithDecorators]
     internal async Task<Result<Guid>> ExecuteAsync(IWorkContext context, CancellationToken ct)
     {
-        //WasValidated();
+        WasValidated();
 
         var entity = new Produto(Nome);
         context.Add(entity);
